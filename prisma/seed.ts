586d3b5c6ae2a0642da3bab5619230453313e67e
george <egorleontev54@gmail.com>
2025-01-15 12:50:53 +0300

    feat: deployment into dockers

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -9,26 +9,30 @@ async function main() {
     const email = 'user@example.com';
     const avatar = '/avatar.png';
 
-    await prisma.$executeRaw`insert into "user" (email, password, avatar) VALUES (${email}, ${hashedPassword}, ${avatar})`;
+    await prisma.$executeRaw`truncate table admin.user cascade`;
+    await prisma.$executeRaw`insert into admin.user (email, password, avatar) VALUES (${email}, ${hashedPassword}, ${avatar})`;
 
     // Create view with count of votes to current feedback for sorting
     await prisma.$executeRaw`
-        create view feedback_vote
+        drop view if exists business.feedback_vote;
+    `;
+    await prisma.$executeRaw`
+        create view business.feedback_vote
             as
         select
             f.*,
             case when v.count is null then 0 ELSE v.count END AS count
-            from feedback f
-            left join (select v.feedback_id, count(*) count from vote v group by v.feedback_id) v on f.id = v.feedback_id
-    `
+            from business.feedback f
+            left join (select v.feedback_id, count(*) count from business.vote v group by v.feedback_id) v on f.id = v.feedback_id
+    `;
 
     // Delete existing records
     await prisma.feedbackStatus.deleteMany({});
     await prisma.feedbackCategory.deleteMany({});
 
     // Reset the sequences
-    await prisma.$executeRaw`ALTER SEQUENCE "feedback_status_id_seq" RESTART WITH 1;`;
-    await prisma.$executeRaw`ALTER SEQUENCE "feedback_category_id_seq" RESTART WITH 1;`;
+    await prisma.$executeRaw`ALTER SEQUENCE dictionary.feedback_status_id_seq RESTART WITH 1;`;
+    await prisma.$executeRaw`ALTER SEQUENCE dictionary.feedback_category_id_seq RESTART WITH 1;`;
 
 
     await prisma.feedbackCategory.createMany({
